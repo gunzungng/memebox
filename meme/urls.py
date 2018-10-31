87a3d07cc053e18a65c5c426bf30514b8862abee
gunzungng <121088879@qq.com>
2018-10-31 08:57:27 +0800

    注册登陆连接

--- meme/urls.py
+++ meme/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from meme import views
 
 urlpatterns = [
-    url(r'^$',views.HomePage)
-
+    url(r'^$',views.HomePage),
+    url(r'^login/$',views.login),
+    url(r'^register/$',views.register)
 
 ]
--- meme/views.py
+++ meme/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 def HomePage(request):
     return render(request,'meme/HomePage.html')
+
+
+def login(request):
+    if request.method=='GET':
+
+        return render(request,'meme/login.html')
+    elif request.method =='POST':
+        pass
+
+
+def register(request):
+    if request.method=='GET':
+
+        return render(request,'meme/register.html')
+    elif request.method =='POST':
+        pass
+    return None
